aba3927ae1caa0a06c7e08b9d18e4aa9720ba489
Albert <alb.02@mail.ru>
2018-03-30 19:46:27 +0500

    Removed gulp tasks

--- GameMaps/gulp/tasksBuild/clean.js
+++
@@ -1,8 +0,0 @@
-const del = require('del');
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-const global = require('../config') ();
-
-module.exports = () => {
-  return del(global.paths.build.base, {force:true});
-}
